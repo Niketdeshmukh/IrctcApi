1696df4c80883149b496e7933af250445c4dc1d4
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2025-01-28 23:46:32 +0530

    added default values

--- models/User.js
+++ models/User.js
@@ -10,9 +10,9 @@ module.exports = (sequelize) => {
 
   User.init(
     {
-      username: { type: DataTypes.STRING, allowNull: false },
+      username: { type: DataTypes.STRING, allowNull: false ,unique: true,},
       password: { type: DataTypes.STRING, allowNull: false },
-      role: { type: DataTypes.STRING, allowNull: false },
+      role: { type: DataTypes.STRING, allowNull: false,defaultValue: 'user', },
     },
     { sequelize, modelName: 'User' }
   );
